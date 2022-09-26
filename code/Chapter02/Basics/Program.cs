8dfc1728173085469698f374924b91c188933c36
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-09-26 08:16:57 +0200

    Numbers

--- code/Chapter02/Basics/Program.cs
+++ code/Chapter02/Basics/Program.cs
@@ -10,25 +10,30 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            System.Data.DataSet ds;
-            System.Net.Http.HttpClient client;
-            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
-            {
-                var a = Assembly.Load(new AssemblyName(r.FullName));
+            // System.Data.DataSet ds;
+            // System.Net.Http.HttpClient client;
+            // foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            // {
+            //     var a = Assembly.Load(new AssemblyName(r.FullName));
 
-                int methodCount = 0;
+            //     int methodCount = 0;
 
-                foreach (var t in a.DefinedTypes)
-                {
-                    methodCount += t.GetMethods().Count();
-                }
+            //     foreach (var t in a.DefinedTypes)
+            //     {
+            //         methodCount += t.GetMethods().Count();
+            //     }
 
-                Console.WriteLine(
-                    "{0:N0} types with {1:N0} methods in {2} assembly.",
-                    arg0: a.DefinedTypes.Count(),
-                    arg1: methodCount,
-                    arg2: r.Name);
-            }
+            //     Console.WriteLine(
+            //         "{0:N0} types with {1:N0} methods in {2} assembly.",
+            //         arg0: a.DefinedTypes.Count(),
+            //         arg1: methodCount,
+            //         arg2: r.Name);
+            // }
+
+            Console.WriteLine("\n-------------------------------------------------------\n");
+
+            double heightInMetres = 1.88;
+            Console.WriteLine($"The variable {nameof(heightInMetres)} has the value {heightInMetres}.");
         }
     }
 }
